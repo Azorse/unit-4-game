e2f6c8bf2d783106b53cb1ec220c788468ba5d18
Dan Schissler <schisslerdg@gmail.com>
2019-10-23 22:52:03 -0700

    finished, known bug when game ends

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,73 +1,91 @@
-var currentScore;
-var targetScore;
-var gamesWon;
-var gamesLost;
+var currentScore = 0;
+var targetScore = 0;
+var gamesWon = 0;
+var gamesLost = 0;
 
+var crystals = {}
 
-
-
-
-function beginGame() {
-    currentScore = Math.floor((Math.random() * 102) + 19)
-
-
-}
 
 
 function reset () {
     currentScore = 0;
-    targetScore = 0;
+    $('#current-score').html('');
+    $('#game-status').html('');
+    
 
+
+}
+
+function gameStatus () {
+    if (currentScore >= targetScore) {
+        endGame();
+    }
+}
+
+function beginGame() {
+    
+    targetScore = Math.floor((Math.random() * 102) + 19)
+    $('#target-score').html(targetScore);
+    console.log(targetScore);
+
+    for (var i = 1; i < 5; i++) {
+        var value = "cry" + i;
+        crystals[value] = Math.floor(Math.random() * 12 + 1);
+
+    }
+    console.log(crystals);
 }
 
 function endGame() {
     if (currentScore === targetScore) {
-        $('#game-status').innerHtml('You Won!');
+        $('#game-status').html('You Won!');
         gamesWon++;
-        $('#wins').innerHtml(gamesWon);
+        $('#wins').html(gamesWon);
+        reset();
       
     } else {
-        $('#game-status').innerHtml('You Lost!');
+        $('#game-status').html('You Lost!');
         gamesLost++;
-        $('#losses').innerHtml(gamesLost);
+        $('#losses').html(gamesLost);
+        reset();
+
     }
-    reset();
+    
+    
 }
 
 $(document).ready(function() {
+
     beginGame();
 
-
-
-
-
-    $('#crystal1').on('click', function() {
-        currentScore += cry1
+    $('#crystal1').click(function() {
+        currentScore += crystals.cry1
+        $('#current-score').html(currentScore);
+        console.log(currentScore)
+        gameStatus();
     });
 
-    $('#crystal2').on('click', function() {
-        currentScore += cry2
+    $('#crystal2').click(function() {
+        currentScore += crystals.cry2
+        $('#current-score').html(currentScore);
+        console.log(currentScore)
+        gameStatus();
     });
 
-    $('#crystal3').on('click', function() {
-        currentScore += cry3
+    $('#crystal3').click(function() {
+        currentScore += crystals.cry3
+        $('#current-score').html(currentScore);
+        console.log(currentScore)
+        gameStatus();
     });
 
-    $('#crystal4').on('click', function() {
-        currentScore += cry4
+    $('#crystal4').click(function() {
+        currentScore += crystals.cry4
+        $('#current-score').html(currentScore);
+        console.log(currentScore)
+        gameStatus();
+      
     });
 
 
-
-    if (currentScore >= targetScore) {
-        endGame();
-    }
 });
-
-
-
-
-
-
-
-
